2033d9a4a5f6eb7fe0b7458a38f1ed6c38feb1e6
Naomi Mango <naomi.mango@gmail.com>
2021-07-22 12:54:37 +0200

    [NEW] Page d'accueil basique

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="header">
+        <!-- BARRE DE NAVIGATION -->
+        <v-app-bar
+        app
+        color="red"
+        dark
+        >
+        <!-- LOGO -->
+            <router-link to="/">
+                <div class="d-flex align-center">
+                    <v-img
+                    alt="Logo de l'entreprise Groupomania"
+                    class="shrink mr-2"
+                    contain
+                    src="../assets/icon-left-font-monochrome-white.png"
+                    transition="scale-transition"
+                    width="100"
+                    />
+                </div>
+            </router-link>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <Header/>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <!-- LOGO -->
+          <v-img :src="require('../assets/icon-left-font.png')" class="my-3" contain height="200"/>
+        </v-col>
+
+        <!-- TITRE -->
+        <v-col class="mb-2">
+          <h1 class="display-2 font-weight-bold mb-3">
+            Bienvenue sur le réseau interne de Groupomania !
+          </h1>
+          <p class="headline font-weight-regular">
+            Échangez en toute liberté entre collègues !
+            <br>Partagez et commentez vos GIFs.
+          </p>
+        </v-col>
+      </v-row>
+      <v-row class="text-center mb-4">
+        <!-- BOUTON INSCRIPTION -->
+        <v-col cols="6">
+          <router-link to="/signup">
+            <v-btn x-large color="red">
+              <span class="mr-2">Inscrivez-vous</span>
+              <v-icon>mdi-account-plus</v-icon>
+            </v-btn>
+          </router-link>
+        </v-col>
+        <!-- BOUTON CONNEXION -->
+        <v-col cols="6">
+          <router-link to="/login">
+            <v-btn x-large color="red">
+              <span class="mr-2">Connectez-vous</span>
+              <v-icon>mdi-login-variant</v-icon>
+            </v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '../components/Header.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      Header
+    },
   }
-}
 </script>
